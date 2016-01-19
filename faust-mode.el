de3015c23aa26e8242e69293f0e2966b329b7dcf
Bart Brouns <bart@magnetophon.nl>
2016-01-19 18:20:52 +0100

    add emails, version, keywords and license

--- faust-mode.el
+++ faust-mode.el
@@ -2,9 +2,25 @@
 
 ;; Copyright (C) 2016 Juan A. Romero
 
-;; Author: Juan A. Romero
-;; Maintainer: Bart Brouns
+;; Author: Juan A. Romero <rukano@gmail.com>
+;; Version: 0.1
+;; Maintainer: Bart Brouns <bart@magnetophon.nl>
 ;; URL: https://github.com/magnetophon/emacs-faust-mode
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 ;;; Commentary:
 
